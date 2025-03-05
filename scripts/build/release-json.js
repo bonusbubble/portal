e260381059f1127160b4b0d45398402fd5f031d7
bonusbubble <22643122+bonusbubble@users.noreply.github.com>
2025-03-05 09:16:36 -0600

    Create `dist/` if non-existent

--- scripts/build/release-json.js
+++ scripts/build/release-json.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const path = require("path");
 
 const {
@@ -5,8 +6,13 @@ const {
     writeJSONFileSync
 } = require("../../src/util");
 
+const DIST_DIR = path.join(__dirname, "..", "..", "dist");
 
 function main() {
+    if (!fs.existsSync(DIST_DIR)) {
+        fs.mkdirSync(DIST_DIR);
+    }
+
     const packageJSONPath = "package.json";
 
     const {
